31a90b4096d02f41d65c7e4da26d783595f37034
JackBelief <tqywork@163.com>
2020-07-05 21:50:29 +0800

    测试提交

---
+++ go_modules_test/go.mod
@@ -0,0 +1,5 @@
+module go_modules_test
+
+go 1.14
+
+require go.uber.org/zap v1.15.0
---
+++ go_modules_test/src/modules_test/zap_test/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"os"
+)
+
+// 生成日志写对象
+func getLogWriter() zapcore.WriteSyncer {
+	file, _ := os.Create("./test.log")
+	return zapcore.AddSync(file)
+}
+
+func Init() {
+	writeSyncer := getLogWriter() // 日志输出位置相关
+	encoder := getEncoder() // 日志的格式相关
+	core := zapcore.NewCore(encoder, writeSyncer, zapcore.DebugLevel)
+
+	logger := zap.New(core)
+	logger = logger
+	//sugarLogger = logger.Sugar()
+}
+
+func getEncoder() zapcore.Encoder {
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder // 人类可读的时间格式
+	//encoderConfig.EncodeTime = zapcore.EpochTimeEncoder
+	encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+	//return zapcore.NewConsoleEncoder(encoderConfig)  // 可读日志
+	return zapcore.NewJSONEncoder(encoderConfig) // json格式日志
+}
+
+
+func main() {
+
+
+
+	fmt.Println("OK")
+
+
+
+
+}
